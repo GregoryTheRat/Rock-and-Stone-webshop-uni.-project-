512d2dcfa8744f73a7444f74010ceba7d07db42e
orospete <oroszi.peter0712@gmail.com>
2023-04-25 18:21:37 +0200

    Új hírdetmények in progress

---
+++ ROR projekt/untitled/app/controllers/newads_controller.rb
@@ -0,0 +1,5 @@
+class NewadsController < ApplicationController
+  def newads
+
+  end
+end
--- ROR projekt/untitled/config/routes.rb
+++ ROR projekt/untitled/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'home', to: "home#home"
   get 'signup', to: "signup#signup"
   get 'login', to: "login#login"
+  get 'newads', to: "newads#newads"
 
   get 'osszes', to: 'adverts#osszes'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
