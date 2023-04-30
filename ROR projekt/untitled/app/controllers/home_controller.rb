b8811f33880242ead0c8ee6387b83174006ceaeb
orospete <oroszi.peter0712@gmail.com>
2023-04-30 12:40:10 +0200

    dd

--- ROR projekt/untitled/app/controllers/home_controller.rb
+++ ROR projekt/untitled/app/controllers/home_controller.rb
@@ -5,20 +5,20 @@ class HomeController < ApplicationController
 
   def litallads
     query_result = ActiveRecord::Base.connection.exec_query("SELECT  *
-  FROM advertises,users,adverts
-  WHERE advertises.User_id=users.id
-  AND advertises.advert_id=adverts.id")
+    FROM advertises,users,adverts
+    WHERE advertises.User_id=users.id
+    AND advertises.advert_id=adverts.id")
 
     @advertsall=query_result.to_a
   end
   def search
     search_term = params['word'] # Get the search term from the form data
     query_result = ActiveRecord::Base.connection.exec_query("SELECT  *
-FROM advertises,users,adverts
-WHERE advertises.User_id=users.id
-  AND advertises.advert_id=adverts.id
-  AND termek_nev LIKE '%#{search_term}%'
-")
+    FROM advertises,users,adverts
+    WHERE advertises.User_id=users.id
+    AND advertises.advert_id=adverts.id
+    AND termek_nev LIKE '%#{search_term}%'
+    ")
     temp= query_result.to_a
 
     @results= temp
